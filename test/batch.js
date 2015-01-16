ececd2e941669d59661b08e622335cf7cc617a84
Eric Willis <eric@ecwillis.com>
2015-01-16 14:11:18 -0800

    refacoring to inject agenda, added a few checks, and job is pulled from mongo to work with multiple machines

--- test/batch.js
+++ test/batch.js
@@ -7,10 +7,13 @@ var Batch = require('../lib/batch.js');
 
 describe('#()', function(){
   it('should instatiate properly', function(){
-    
-    var a = Batch('localhost:27017/batch-agenda-tests');
 
-    expect(a).toBeA('AgendaBatch');
+    var a = Batch('localhost:27017/batch-agenda-tests');
+    expect(a).toBeA('object');
+
+    var objKeys = Object.keys( a );
+    expect(objKeys).toEqual(["batch","define","cleanBatch","agendaModule","batchHash"]);
+
   });
 
 });
